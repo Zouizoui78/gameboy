f0f21ab4bd8bfc69dff6b7c67462879b21842559
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2022-11-20 00:39:24 +0100

    Moved some files to resources directory

--- src/main.cpp
+++ src/main.cpp
@@ -13,14 +13,14 @@ int main() {
 
     CPU cpu;
 
-    if (!cpu.load_instructions("opcodes.json")) {
+    if (!cpu.load_instructions("resources/opcodes.json")) {
         exit(1);
     }
 
-    if (cpu.load_boot_rom("DMG_ROM.bin"))
+    if (cpu.load_boot_rom("resources/DMG_ROM.bin"))
         cpu.log_boot_rom();
 
-    if (cpu.load_rom("Tetris.gb")) {
+    if (cpu.load_rom("resources/Tetris.gb")) {
         cpu.log_memory(0x0100, 0x0103);
         cpu.log_memory(0x0150, 0x0160);
         cpu.log_memory(0x028b, 0x028f);
